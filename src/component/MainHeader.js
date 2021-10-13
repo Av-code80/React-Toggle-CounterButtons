8adf2a8e28ce751ad5fd59d12612caf56a2e7991
avehghasemian <av.ghasemian@gmail.com>
2021-10-13 08:54:20 +0200

    IconHeader

--- src/component/MainHeader.js
+++ src/component/MainHeader.js
@@ -1,17 +1,21 @@
 import React from 'react'
+
+import { IconContext } from "react-icons";
+import { FaBars } from "react-icons/fa";
 import styles from './MainHeader.module.css'
+
+
 
 const MainHeader = ({headerCounting}) => {
 
     return (
       <>
         <header className={styles.headerButton}>
+          <IconContext.Provider value={{ color: "#ffffff" }}>
+            <FaBars className={styles.faBars} />
+          </IconContext.Provider>
           <h1>React Button Project</h1>
-          <div className={styles.cart}>
-            <span>
-              {headerCounting}
-            </span>
-          </div>
+          <span className={styles.cart}>{headerCounting}</span>
         </header>
       </>
     );
